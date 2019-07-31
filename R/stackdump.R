c843904091f22eb23a1ce3bdd458b7d2a53e6187
Joe Cheng <joe@rstudio.com>
2019-07-31 13:07:48 -0700

    Print pid

--- R/stackdump.R
+++ R/stackdump.R
@@ -1,9 +1,9 @@
 # Very dangerous to pass quit=FALSE. Don't trust the process after that!
-init <- function(silent=FALSE, quit=TRUE) {
+init <- function(silent=FALSE, quit=FALSE) {
   .Call(registerSigQuitHandler, quit)
   if (!isTRUE(silent)) {
     maybeExit <- ifelse(isTRUE(quit), " and exit", "")
-    message("Stack dump handler attached.\nSend SIGQUIT to print stack trace", maybeExit, ".")
+    message("Stack dump handler attached.\nRun `kill -SIGQUIT ", Sys.getpid(), "` in a terminal to print a stack trace", maybeExit, ".")
   }
   invisible()
 }
